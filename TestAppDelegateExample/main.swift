a5251549b84aeabe52ea3f0f6b22557394edb621
Andrés P <Tovkal@gmail.com>
2017-03-05 11:06:34 +0100

    Update to Swift 3 (#1)
    * Update to Swift 3
    
    * Forgot to use delegateClassName()
    
    Thanks @Tovkal

--- TestAppDelegateExample/main.swift
+++ TestAppDelegateExample/main.swift
@@ -5,8 +5,8 @@
 import UIKit
 
 private func delegateClassName() -> String? {
-    return NSClassFromString("XCTestCase") == nil ? NSStringFromClass(AppDelegate) : nil
+    return NSClassFromString("XCTestCase") == nil ? NSStringFromClass(AppDelegate.self) : nil
 }
 
 
-UIApplicationMain(Process.argc, Process.unsafeArgv, nil, delegateClassName())
+UIApplicationMain(CommandLine.argc, UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc)), nil, delegateClassName())
